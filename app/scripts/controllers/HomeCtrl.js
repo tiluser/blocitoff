30f6298cd69a21922a68278ba486b21e6f7c497c
Joseph M. O'Connor <tiluser@yahoo.com>
2018-09-22 22:40:03 -0400

    Minor cleanup

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -3,14 +3,9 @@
     function HomeCtrl($window, Task) {
         this.title = "- Home page";
         this.tasks = Task.all;
-        this.message = function(msg) {
-            $window.alert(msg);
-        };
         this.addTask = function(description, priority) {
-         //   Task.add();
             Task.add(description, priority);
             $window.alert("Task added");
-           // $window.alert(Task.taskDate);
         };
 
         // Set task to 'I' or inactive if it's past expiration date
@@ -30,15 +25,6 @@
              alert("Test of defining Creole primitive in a controlloer");
         });
         cfb1.BuildPrimitive("HTEST", cfb1.Modules.AppSpec.doHomeTest, "AppSpec.doHomeTest", "FORTH", "COMPINPF","( -- ) Test of defining Creole Primitive in a controller");
-        this.doit = function (taskId) {
-            Task.doit(taskId);
-        };
-        // at the bottom of your controller
-        var init = function () {   
-         //   Task.doit();
-        };
-        // and fire it after definition
-        init();
     }
 
     angular
